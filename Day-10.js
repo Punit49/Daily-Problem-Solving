932bf6b9c8eafff833d44289ad985a9a6a8554d9
Punit Sahu <therealpunit5@gmail.com>
2026-01-24 17:23:35 +0530

    Day-10 Implement largest character finding code

---
+++ Day-10.js
@@ -0,0 +1,13 @@
+// ? Q. Find largest character in a string.
+function largestChar(str){
+    let largest = str[0];
+    for (const char of str) {
+        if(char > largest) {
+            largest = char;
+        }
+    }
+    return largest;
+}
+
+// Example - 
+console.log(largestChar("abczdef"));
