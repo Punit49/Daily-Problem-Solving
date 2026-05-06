3b6ed0e28cb32aec9b185c0478531d3fcb4c9e70
Punit49 <therealpunit5@gmail.com>
2026-05-06 21:27:02 +0530

    Selection Sort Ascending order array sorting solution

---
+++ Week-13/day85.js
@@ -0,0 +1,37 @@
+// ======================================================
+// week-12, Day-84 of daily problem solving
+// ======================================================
+
+// ------------------------------------------------------
+// Q. Write a function to Sort an Array in Ascending Order Using Selection Sort
+// ------------------------------------------------------
+
+function selectionSortAsc(arr) {
+  if(!arr || !Array.isArray(arr)){
+    return [];
+  }
+
+  let min;
+
+  for(let i = 0; i < arr.length - 1; i++){
+    min = i;
+
+    for(let j = i + 1; j < arr.length; j++){
+      if(arr[j] < arr[min]){
+        min = j;
+      }
+    }
+    
+    [arr[i], arr[min]] = [arr[min], arr[i]];  
+  }
+  
+  return arr;
+}
+
+function main(){
+  const sortedArr = selectionSortAsc([7, 2, 9, 4, 1]);
+  console.log(sortedArr);
+}
+
+main();
+ 
